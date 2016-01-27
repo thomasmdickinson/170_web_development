ee8bf25bf4525e29ffa707affa19e40d04413f29
Tom Dickinson <tom@thomasmdickinson.com>
2016-01-26 21:40:43 -0500

    add validation

--- todo.rb
+++ todo.rb
@@ -2,6 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+# View list of all lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Renders the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Creates a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  if (1..100).cover? list_name.size
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    erb :new_list, layout: :layout
+    session[:error] = "List name must be between 1 and 100 characters."
+    redirect "/lists/new"
+  end
 end
